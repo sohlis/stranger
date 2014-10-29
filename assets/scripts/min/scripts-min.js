3fd8fbfa0b1cceef02438243402480bea7a7277c
Stefan Sohlstrom <stefan.sohlstrom@gmail.com>
2014-10-28 17:48:07 -0700

    changed color of logo

---
+++ assets/scripts/min/scripts-min.js
@@ -0,0 +1,1 @@
+function fullscreen(){var e=$(".masthead"),i=$(window).height(),l=$(window).width();e.width(l),e.height(i)}fullscreen(),$(window).resize(fullscreen),$(window).scroll(headerParallax);
